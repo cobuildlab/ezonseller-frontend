4612ee339e2e3da877ff89893dcbb84c001410ce
Jorge Pernia Godoy <jorpg23@gmail.com>
2018-01-26 14:09:04 -0400

    fix col search

--- src/SharePage/SharePage.jsx
+++ src/SharePage/SharePage.jsx
@@ -30,51 +30,64 @@ class SharePage extends React.Component {
             <div>
                 <Header url={this.props} />
                 {ebay.items &&
-                    <div className="col-12 section-data">
+                    <div className="col-12">
                         <div className="row">
-                            <div className="col-12 section-data">
-                                <div className="row">
-                                    <div className="col-md-12">
-                                        <div className="content-data">
-                                        <h2>By Amazon Result</h2>
-                                        <div className="col-md-4 col-xs-12 no-padding">
-                                            <div className="card height-card">
-                                            <img className="card-img-top" height="250" src={objectProduct.large_image_url} alt="product ebay"/>
-                                            <div className="card-body">
-                                                <h3 className="card-title title-title">{objectProduct.title}</h3>
-                                                <h5>{objectProduct.asin}</h5>
-                                                <h5>{objectProduct.country}</h5>
-                                                <h5>{objectProduct.price_and_currency}</h5>
-                                            </div>
-                                            </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                          <div className="col-md-12 section-data">
+                              <div>
+                              <h2>By Amazon Result</h2>
+                              <div className="col-md-4 col-xs-12">
+                                  <div className="card height-card">
+                                    <div className="card-img-top" style={
+                                                {  'backgroundImage': 'url( ' + objectProduct.large_image_url + ')',
+                                                  'backgroundRepeat': 'no-repeat',
+                                                  'backgroundPosition': 'center',
+                                                  'backgroundSize': 'contain',
+                                                  'width': '100%',
+                                                  'height': '250px',
+                                                }
+                                              } ></div>
+                                  <div className="card-body">
+                                      <h3 className="card-title title-title">{objectProduct.title}</h3>
+                                      <h5>{objectProduct.asin}</h5>
+                                      <h5>{objectProduct.country}</h5>
+                                      <h5>{objectProduct.price_and_currency}</h5>
+                                  </div>
+                                  </div>
                                 </div>
-                            </div>
-                            <div className="col-md-12">
-                                
-                                <div className="content-data">
-                                    <h2>By Ebay Result</h2>
-                                    {ebay.items.slice(0, 5).map((item, index) =>
-                                        <div key={index} className="col-md-4 col-xs-12 no-padding">
-                                            <div className="card height-card">
-                                                <img className="card-img-top" height="250" src={item.galleryURL} alt="product ebay"/>
-                                                <div className="card-body">
-                                                    <h3 className="card-title title-title">{item.title}</h3>
-                                                    <h5>{item.location}</h5>
-                                                    <h5>{item.country}</h5>
-                                                    <h5>{item.itemId}</h5>
-                                                    <h5>{item.sellingStatus.currency} {item.sellingStatus.values}</h5>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    )}
-                                </div>
-                            </div>
+                              </div>
+                          </div>
+                          <div className="col-12">
+                            <h2>By Ebay Result</h2>
+                          </div>
+                            {ebay.items.slice(0, 5).map((item, index) =>
+
+                                  <div key={index} className="col-md-4">
+                                      <div className="card height-card">
+                                        <div className="card-img-top" style={
+                                            {  'backgroundImage': 'url( ' + item.galleryURL + ')',
+                                              'backgroundRepeat': 'no-repeat',
+                                              'backgroundPosition': 'center',
+                                              'backgroundSize': 'contain',
+                                              'width': '100%',
+                                              'height': '250px',
+                                            }
+                                          } ></div>
+                                          <div className="card-body">
+                                              <h3 className="card-title title-title">{item.title}</h3>
+                                              <h5>{item.location}</h5>
+                                              <h5>{item.country}</h5>
+                                              <h5>{item.itemId}</h5>
+                                              <h5>{item.sellingStatus.currency} {item.sellingStatus.values}</h5>
+                                          </div>
+                                      </div>
+                                  </div>
+
+                                )}
+
+
                         </div>
                 </div>
-                }   
+                }
         </div>
         );
     }
@@ -85,7 +98,7 @@ function mapStateToProps(state) {
     const { loggingIn } = state.authentication;
     return {
         loggingIn,
-        state, 
+        state,
         ebay
     };
 }
