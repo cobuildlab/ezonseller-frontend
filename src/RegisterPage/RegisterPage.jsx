e2c077f6221d040bc28b2e204972f245ee8d7319
Edgar Espinetti <eespinetti@4geeks.com.ve>
2018-02-02 16:46:14 -0300

    captcha display

--- src/RegisterPage/RegisterPage.jsx
+++ src/RegisterPage/RegisterPage.jsx
@@ -42,10 +42,12 @@ class RegisterPage extends React.Component {
             }
         });
     }
-
     componentDidMount(){
         $().ready(function() {
             $(".fakeloader").fadeOut();
+            console.log($("iframe"));
+            $("iframe").width(374);
+            $("iframe").height(526);
         });
     }
 
@@ -87,92 +89,90 @@ class RegisterPage extends React.Component {
             <div className="col-md-12 no-padding">
                 <img src={Logo} className="logo-img center-block img-fluid" alt="" />
                 <div className="row top d-flex justify-content-center">
-                <div className="col-md-8 col-10 section-register">
-                    <h2 className="bottom-title">Register</h2>
-                    <form name="form" id="commentForm" onSubmit={this.handleSubmit}>
+                    <div className="col-md-8 col-10 section-register">
+                        <h2 className="bottom-title">Register</h2>
+                        <form name="form" id="commentForm" onSubmit={this.handleSubmit}>
 
-                        <div className="row">
-                          <div className="col-md-6">
-                            <div className="form-group">
-                                <label htmlFor="firstName">First Name</label>
-                                <input type="text" className="form-control rigth-input" name="first_name" value={user.first_name} onChange={this.handleChange} required />
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label htmlFor="firstName">First Name</label>
+                                        <input type="text" className="form-control rigth-input" name="first_name" value={user.first_name} onChange={this.handleChange} required />
 
-                            </div>
-                          </div>
-                          <div className="col-md-6">
-                            <div className="form-group">
-                                <label htmlFor="lastName">Last Name</label>
-                                <input type="text" className="form-control" name="last_name" value={user.last_name} onChange={this.handleChange} required />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label htmlFor="lastName">Last Name</label>
+                                        <input type="text" className="form-control" name="last_name" value={user.last_name} onChange={this.handleChange} required />
 
-                            </div>
-                          </div>
-                        </div>
-
-                        <div className="row">
-                          <div className="col-md-6">
-                            <div className="form-group">
-                                <label htmlFor="username">Username</label>
-                                <input type="text" className="form-control rigth-input" name="username" value={user.username} onChange={this.handleChange} required />
-                            </div>
-                          </div>
-                          <div className="col-md-6">
-                            <div className="form-group">
-                                <label htmlFor="email">Email</label>
-                                <input type="email" className="form-control" name="email" value={user.email} onChange={this.handleChange} required />
-                            </div>
-                          </div>
-                        </div>
-
-                        <div className="row">
-                          <div className="col-md-6">
-                            <div className="form-group">
-                                <label htmlFor="password">Password</label>
-                                <input type="password" className="form-control" id="password" name="password" value={user.password} onChange={this.handleChange} required />
-                            </div>
-                          </div>
-                          <div className="col-md-6">
-                            <div className="form-group">
-                                <label htmlFor="password">Confirm Password</label>
-                                <input type="password" className="form-control" name="confirm_password" value={user.confirm_password} onChange={this.handleChange} required />
-                            </div>
-                          </div>
-                        </div>
-
-                        <div className="row">
-                          <div className="col-md-12">
-                            <div className='form-group'>
-                                <div className="row">
-                                    <div className="col-md-12 no-padding">
-                                    <input type="radio" className="" name="terms" onChange={this.handleChange} required />
-                                    <input type="hidden" name="register_form" id="register_form" value={user.register_form} />
-                                     <Link to="/terms"  target="_blank" className="btn btn-link term">I Accept the Terms and Conditions Ezonseller.</Link>
                                     </div>
                                 </div>
                             </div>
-                          </div>
-                          <div className="Col">
-                          <Recaptcha
-                            sitekey='6LejRUMUAAAAAEmqctY7MvmGQ3_AAvKcuvYKBU0x'
-                            callback={verifyCallback}
-                            expiredCallback={expiredCallback}
-                            locale="gb-EN"
-                            className="customClassName"
 
-                            // Other props will be passed into the component.
-                            data-theme=""
-                            />
- 
-                          </div>
-                        </div>
-                        <div className="form-group d-flex justify-content-center">
-                            <button className="btn btn-primary" id="create_account">Create Account</button>
-                            {registering &&
-                                <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" alt="logo" />
-                            }
-                            <Link to="/login" className="btn btn-danger">Cancel</Link>
-                        </div>
-                    </form>
-                </div>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label htmlFor="username">Username</label>
+                                        <input type="text" className="form-control rigth-input" name="username" value={user.username} onChange={this.handleChange} required />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <input type="email" className="form-control" name="email" value={user.email} onChange={this.handleChange} required />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label htmlFor="password">Password</label>
+                                        <input type="password" className="form-control" id="password" name="password" value={user.password} onChange={this.handleChange} required />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label htmlFor="password">Confirm Password</label>
+                                        <input type="password" className="form-control" name="confirm_password" value={user.confirm_password} onChange={this.handleChange} required />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                            <div className="col-md-12">
+                                <div className='form-group'>
+                                    <div className="col-md-12 no-padding">
+                                        <input type="radio" className="" name="terms" onChange={this.handleChange} required />
+                                        <input type="hidden" name="register_form" id="register_form" value={user.register_form} />
+                                        <Link to="/terms"  target="_blank" className="btn btn-link term">I Accept the Terms and Conditions Ezonseller.</Link>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <Recaptcha
+                                sitekey='6LejRUMUAAAAAEmqctY7MvmGQ3_AAvKcuvYKBU0x'
+                                callback={verifyCallback}
+                                expiredCallback={expiredCallback}
+                                locale="gb-EN"
+                                className="customClassName"
+                                data-theme="light"
+
+                                // Other props will be passed into the component.
+                                />
+
+    
+                            </div>
+                            <div className="form-group d-flex justify-content-center">
+                                <button className="btn btn-primary" id="create_account">Create Account</button>
+                                {registering &&
+                                    <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" alt="logo" />
+                                }
+                                <Link to="/login" className="btn btn-danger">Cancel</Link>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
             );
