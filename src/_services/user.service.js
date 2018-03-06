fc85d5cb6832f3bf9121a6f6d7722ee4592065b9
Edgar Espinetti <eespinetti_99@hotmail.com>
2018-03-06 11:08:18 -0300

    eliminando constante

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -35,10 +35,7 @@ export const userService = {
     histoyPayment
 };
 
-//const URL = "https://ezonseller-backend.herokuapp.com/";
-
 const URL = "https://ezonsellerbackend.herokuapp.com/";
-//const URL = "http://192.168.0.8:8000/";
 
 function login(username, password) {
     const requestOptions = {
